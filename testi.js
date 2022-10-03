651d6222d1c14b6ff60e936925aa1ceed17514a2
torastrengel <heikki@heikki.codes>
2022-10-03 13:39:58 +0300

    Initial commit

---
+++ testi.js
@@ -0,0 +1,9 @@
+// Ohjelma "syötit luvun, joka on pienempi kuin 100"
+
+const smallerThanHundred = (num) => {
+  if (num < 100) {
+    return '';
+  }
+
+  return 'Syötit luvun, joka on suurempi kuin 100';
+};
