59ff2124450a23e2a6d5a1d2711cee67728a3415
torastrengel <heikki@heikki.codes>
2022-12-01 21:30:34 +0200

    Tsekkaa onko route admineille

--- tenttisovellus/src/ProtectedRoute.jsx
+++ tenttisovellus/src/ProtectedRoute.jsx
@@ -1,8 +1,26 @@
+import { useContext } from 'react';
 import { Navigate } from 'react-router-dom';
+import { UserContext } from './context/UserContext';
 
-const ProtectedRoute = ({ children, token, redirectPath = '/' }) => {
+const ProtectedRoute = ({
+  children,
+  token,
+  isAdminRoute = false,
+  redirectPath = '/',
+}) => {
+  const { user } = useContext(UserContext);
   if (!token) {
-    return <Navigate to={redirectPath} replace />;
+    return <Navigate to={redirectPath} />;
+  }
+
+  if (isAdminRoute && token) {
+    if (user.isAdmin === 'true') {
+      console.log('Admin oikeudet, teretulemast!');
+      return children;
+    } else {
+      console.log('Ei admin oikeuksia!');
+      return <Navigate to={redirectPath} />;
+    }
   }
 
   return children;
