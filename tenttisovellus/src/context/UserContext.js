fd6b96bc01e56659435f178c3016c54511c1417f
torastrengel <heikki@heikki.codes>
2022-12-01 21:31:23 +0200

    Luo oma konteksti käyttäjille

---
+++ tenttisovellus/src/context/UserContext.js
@@ -0,0 +1,22 @@
+import { createContext, useState } from 'react';
+
+export const UserContext = createContext(null);
+
+export const UserContextProvider = ({ children }) => {
+  const [user, setUser] = useState({
+    userId: localStorage.getItem('tenttisovellus_userId'),
+    email: localStorage.getItem('tenttisovellus_user_email'),
+    isAdmin: localStorage.getItem('tenttisovellus_user_is_admin'),
+    token: localStorage.getItem('tenttisovellus_token'),
+  });
+  const [isAuth, setIsAuth] = useState(false);
+
+  const value = {
+    user,
+    setUser,
+    isAuth,
+    setIsAuth,
+  };
+
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+};
