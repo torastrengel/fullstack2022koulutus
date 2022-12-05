8cf0dca4900fd8675e75604d435788271e68f9e8
torastrengel <heikki@heikki.codes>
2022-12-05 18:03:50 +0200

    Poista yksittäinen kysymys listoilta

--- tenttisovellus/src/TenttiLista.jsx
+++ tenttisovellus/src/TenttiLista.jsx
@@ -1,29 +1,42 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+
+import { TenttiContext } from './context/TenttiContext';
 
 import Button from '@mui/material/Button';
 
 const TenttiLista = () => {
-  const [tenttiNapit, setTenttiNapit] = useState([]);
+  const { dispatch, tentti } = useContext(TenttiContext);
 
   useEffect(() => {
     const haeTentit = async () => {
       const { data } = await axios.get('https://localhost:3001/tentit');
-      const result = data.map((item) => (
-        <Link key={item.id} to={`/tentit/${item.id}`}>
-          <Button>{item.nimi}</Button>
-        </Link>
-      ));
-      setTenttiNapit(result);
+      if (data.success) {
+        dispatch({
+          type: 'HAE_KAIKKI_TENTIT',
+          payload: {
+            tentit: data.results,
+          },
+        });
+      } else {
+        return <h1>Tenttien haussa virhe!</h1>;
+      }
     };
     haeTentit();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <div>
       <h1>Valittavissa olevat tentit:</h1>
-      <div className="tenttinapit-container">{tenttiNapit}</div>
+      <div className="tenttinapit-container">
+        {tentti.tenttilista?.map((item) => (
+          <Link key={item.id} to={`/tentit/${item.id}`}>
+            <Button>{item.nimi}</Button>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
